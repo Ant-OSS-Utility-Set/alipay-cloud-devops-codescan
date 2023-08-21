f25328181532dff9e706c149d2551e81ddf02d50
殷振南 <yzn01714396@antgroup.com>
2023-08-21 14:13:11 +0800

    1. 开源合规支持

---
+++ src/jobprocessors/codescanScaProcessor.js
@@ -0,0 +1,35 @@
+const core = require("@actions/core");
+
+/**
+ * 处理开源合规的扫描结果 返回ture表示有报错
+ * 警告:
+ *   1. 代码相似度过高
+ * 报错:
+ *   1. 存在licence冲突
+ */
+function process(jobDetail){
+    const artifacts = JSON.parse(jobDetail.artifacts);
+    const licence = artifacts.license;
+    let failed = false
+
+    //licence冲突 报错
+    const licenceText = JSON.parse(licence.text);
+    if (licenceText.compatibility === false) {
+        Object.entries(licenceText.conflictingLicense).forEach(([licenceName, component]) => {
+            if (licenceName === "") {
+                return;
+            }
+            const [componentName, version] = Object.entries(component)[0];
+            core.setFailed(`请注意, 项目依赖的 ${componentName}:${version} 组件,使用的licence可能与本项目冲突: ${licenceName}`)
+            failed = true;
+        });
+    }
+    //code相似 警告
+    const code = artifacts.code;
+    const codeText = JSON.parse(code.text);
+    Object.entries(codeText).forEach(([fileName, conflictRepo]) => {
+        core.warning(`请注意, 您的代码 ${fileName} 与 开源项目 ${conflictRepo[0].name}:${conflictRepo[0].version} 的文件: ${conflictRepo[0].downloadUrl} 相似度: ${conflictRepo[0].score}`)
+    });
+    return ! licenceText.compatibility;
+}
+module.exports = process;
---
+++ src/jobprocessors/stcProcessor.js
@@ -0,0 +1,24 @@
+const core = require("@actions/core");
+
+/**
+ * 处理安全扫描里的安全风险 返回ture表示有报错
+ * 报错:
+ *   1. high/urgent 级别的安全隐患
+ * 警告:
+ *   1. low/medium/warn 级别的安全隐患
+ */
+function process(jobDetail){
+    console.log("stc processor")
+    //高等级 报错
+    const highAndUrgent = [...JSON.parse(jobDetail.high), ...JSON.parse(jobDetail.urgent)];
+    highAndUrgent.map(item => item.title).forEach(errorMessage=>{
+        core.setFailed(errorMessage)
+    });
+    //低等级 警告
+    const warningRisks = [...JSON.parse(jobDetail.low), ...JSON.parse(jobDetail.medium), ...JSON.parse(jobDetail.warn)];
+    warningRisks.map(item => item.title).forEach(errorMessage=>{
+        core.warning(errorMessage)
+    });
+    return highAndUrgent.length > 0;
+}
+module.exports = process;
