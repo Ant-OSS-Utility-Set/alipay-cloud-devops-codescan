000d8cc4133bc64c74ec9e30d2fde5a3a5711cc7
殷振南 <yzn01714396@antgroup.com>
2023-08-23 10:30:15 +0800

    1. 开源合规支持

--- dist/index.js
+++ dist/index.js
@@ -4995,6 +4995,10 @@ const repoTemplates = {
         projectId: "5603361",
         templateId: 5603652
     },
+    "xuqiu/sofa-rpc": {
+        projectId: "5600832",
+        templateId: 5600545
+    },
 //以上为测试项目
     "sofastack/sofa-rpc": {
         projectId: "5600832",
