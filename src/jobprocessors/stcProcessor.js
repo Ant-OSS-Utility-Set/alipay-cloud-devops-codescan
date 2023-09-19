e161e1c7e8fb1f91b3c178356a720b418b8d5391
殷振南 <yzn01714396@antgroup.com>
2023-09-19 14:10:17 +0800

    1. 安全扫描丰富信息

--- src/jobprocessors/stcProcessor.js
+++ src/jobprocessors/stcProcessor.js
@@ -8,21 +8,8 @@ const core = require("@actions/core");
  *   1. low/medium/warn 级别的安全隐患
  */
 function process(jobDetail){
-    //高等级 报错
-    const highAndUrgent = [...JSON.parse(jobDetail.high), ...JSON.parse(jobDetail.urgent)];
-    highAndUrgent.forEach(risk=>{
-        let errorMessage = risk.title;
-        if (risk.filePath) {
-            errorMessage += `\n文件: ${risk.filePath}`
-        }
-        if (risk.description) {
-            errorMessage += `修复建议: ${risk.description}`
-        }
-        core.setFailed(errorMessage);
-    });
-    //低等级 警告
-    const warningRisks = [...JSON.parse(jobDetail.low), ...JSON.parse(jobDetail.medium), ...JSON.parse(jobDetail.warn)];
-    warningRisks.forEach(risk=>{
+    let hasError = false;
+    [...JSON.parse(jobDetail.high), ...JSON.parse(jobDetail.urgent),...JSON.parse(jobDetail.low), ...JSON.parse(jobDetail.medium), ...JSON.parse(jobDetail.warn)].forEach(risk=>{
         let errorMessage = risk.title;
         if (risk.filePath) {
             errorMessage += `\n文件: ${risk.filePath}`
@@ -30,8 +17,13 @@ function process(jobDetail){
         if (risk.description) {
             errorMessage += `\n细节/建议: ${risk.description}`
         }
-        core.warning(errorMessage);
+        if (['high','urgent'].includes(risk.rank)){
+            hasError = true;
+            core.setFailed(errorMessage);
+        }else {
+            core.warning(errorMessage);
+        }
     });
-    return highAndUrgent.length > 0;
+    return hasError;
 }
 module.exports = process;
