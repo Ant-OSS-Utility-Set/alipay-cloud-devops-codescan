f53bbaf7d992de68d7d3f8876c9c7c9b65d85e23
duanwenlong <WB-dwl906436>
2023-11-01 17:43:47 +0800

    跳转小程序云测试

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ async function getStarted(branch, codeRepo, codeType) {
             });
             core.debug("jobResponse.data: " + JSON.stringify(jobResponse.data))
             const link = `https://devops.cloud.alipay.com/project/5603361/${jobId}/pipeline/details`;
-            core.error(["详情查看:"]({link}));
+            core.error(`详情请查看：${link}`);
             const jobDetail = jobResponse.data.result.data;
             const jobProcessor = jobProcessors[failureJob.componentName];
             if (jobDetail) {
