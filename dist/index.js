596a8be5833d3cf351d1f7b6844e941a00f2ea67
duanwenlong <WB-dwl906436>
2023-11-02 10:26:46 +0800

    修改触发类型完成

--- dist/index.js
+++ dist/index.js
@@ -32589,7 +32589,7 @@ async function getStarted(branch, codeRepo, codeType) {
                 headers: headers
             });
             core.debug("jobResponse.data: " + JSON.stringify(jobResponse.data))
-            const link = `https://devops.cloud.alipay.com/project/5603361/${recordId}//pipeline/details`;
+            const link = `https://devops.cloud.alipay.com/project/5603361/${recordId}/pipeline/details`;
             core.error(`详情请查看：${link}`);
             const jobDetail = jobResponse.data.result.data;
             const jobProcessor = jobProcessors[failureJob.componentName];
